7b82acff8a1affb166e26b0b6652b342de7b12c4
Jacopo Rizzo <jrizzo@sissa.it>
2022-09-27 22:28:09 +0200

    Added test file

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-import numpy
+import numpy as np
+import scipy
+from scipy.stats import norm
 
-print('done')
+def get_gaussian_sample(mu, sigma, N):
+    rv1 = norm(loc=mu, scale=sigma)
+    sample = rv1.rvs(N)
+    return sample
+
+def compute_sample_average(sample):
+    return sample.mean()
+
+def compute_sample_std(sample):
+    return np.sqrt(np.var(sample))
+
+sample = get_gaussian_sample(mu=1,sigma=1,N=1000)
+print(compute_sample_average(sample))
+print(compute_sample_std(sample))
---
+++ test_pyfile.py
@@ -0,0 +1,8 @@
+from main import *
+
+def test_var_is_positive():
+    sample = get_gaussian_sample(mu=1,sigma=1,N=1000)
+    std = compute_sample_std(sample)
+    assert std < 0
+    
+#test_var_is_positive()
