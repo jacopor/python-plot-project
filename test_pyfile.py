4dea24f1b9ccd903c3269e1faec83678f1328411
jacopor <60133535+jacopor@users.noreply.github.com>
2022-09-28 00:04:55 +0200

    Update test_pyfile.py

--- test_pyfile.py
+++ test_pyfile.py
@@ -3,7 +3,7 @@ from main import *
 def test_var_is_positive():
     sample = get_gaussian_sample(mu=1,sigma=1,N=1000)
     std = compute_sample_std(sample)
-    assert std < 0
+    assert std > 0
     
 #test_var_is_positive()
 
